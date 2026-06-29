7e9a2571e77153ea59e0f7018a4e6db1ead44f7e
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 00:16:15 +0600

    feat: create simple page layout with headerbar

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { Appbar, Surface, Text } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
+  const insets = useSafeAreaInsets();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Surface style={[styles.rootSurface, { paddingBottom: insets.bottom }]}>
+      <Appbar.Header>
+        <Appbar.Content title="Transactions" />
+      </Appbar.Header>
+
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </Surface>
   );
 }
+
+const styles = StyleSheet.create({
+  rootSurface: { flex: 1 },
+});
