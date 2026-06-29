1daa013f4bac37deb33204559d824168d2d201d2
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 00:11:47 +0600

    feat: remove headerbar from the index page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,9 @@ import { PaperProvider } from "react-native-paper";
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
     </PaperProvider>
   );
 }
