217997a90e5c99a03f3e0d326edd401c4acf0930
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 22:53:36 +0600

    feat: implement date picking function
    Now the date picker works.

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { Pressable, View } from "react-native";
 import { TextInput } from "react-native-paper";
+import {
+  DatePickerModal,
+  enGB,
+  registerTranslation,
+} from "react-native-paper-dates";
+registerTranslation("en-GB", enGB);
 
 const DatePicker = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [value, setValue] = useState<Date | undefined>(undefined);
+
+  const formatDate = (date: Date | undefined) => {
+    if (!date) return "";
+    return date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
+  const handleConfirm = (params: { date: Date | undefined }) => {
+    setIsVisible(false);
+    setValue(params.date);
+  };
+
   return (
     <View>
-      <Pressable>
+      <Pressable onPress={() => setIsVisible(true)}>
         <View>
           <TextInput
             label={"Date"}
+            value={formatDate(value)}
             mode="outlined"
             placeholder="Select Date"
             left={<TextInput.Icon icon={"calendar"} />}
@@ -15,6 +40,15 @@ const DatePicker = () => {
           />
         </View>
       </Pressable>
+
+      <DatePickerModal
+        locale="en-GB"
+        mode="single"
+        visible={isVisible}
+        date={value}
+        onDismiss={() => setIsVisible(false)}
+        onConfirm={handleConfirm}
+      />
     </View>
   );
 };
