b264cade5a112c9b29beefd2f9477b4f2e2ca190
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 08:49:43 +0600

    feat: add new-transaction page to the layout stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
     <PaperProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="new-transaction" options={{ headerShown: false }} />
       </Stack>
     </PaperProvider>
   );
