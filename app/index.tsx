33cdf1034f8ab1ad8d63ebd789ca2dfa0f366f60
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 08:30:54 +0600

    feat: create floating action button
    This button will be used for new transaction.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Appbar, Surface, Text } from "react-native-paper";
+import { AnimatedFAB, Appbar, Surface, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const insets = useSafeAreaInsets();
+  const [isExtended, setIsExtended] = useState(true);
+
   return (
     <Surface style={[styles.rootSurface, { paddingBottom: insets.bottom }]}>
       <Appbar.Header>
@@ -11,10 +14,25 @@ export default function Index() {
       </Appbar.Header>
 
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <AnimatedFAB
+        icon={"plus"}
+        label={"New Transaction"}
+        extended={isExtended}
+        onPress={() => console.log("pressed")}
+        animateFrom={"right"}
+        iconMode={"dynamic"}
+        style={styles.fabStyle}
+      />
     </Surface>
   );
 }
 
 const styles = StyleSheet.create({
   rootSurface: { flex: 1 },
+
+  fabStyle: {
+    bottom: 100,
+    right: 16,
+    position: "absolute",
+  },
 });
