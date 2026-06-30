e924addc423bdc87aba614ebe7ed42085a77824f
Md. Jobayed Islam <contact@mjobayed.com>
2026-06-30 09:06:56 +0600

    feat: add AppBar to the new-transaction page
    This headerbar includes a back button and a title.

--- app/new-transaction.tsx
+++ app/new-transaction.tsx
@@ -1,12 +1,18 @@
+import { useRouter } from "expo-router";
 import { StyleSheet } from "react-native";
-import { Surface, Text } from "react-native-paper";
+import { Appbar, Surface, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const NewTransaction = () => {
   const insets = useSafeAreaInsets();
+  const router = useRouter();
 
   return (
     <Surface style={[styles.rootSurface, { paddingBottom: insets.bottom }]}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => router.back()} />
+        <Appbar.Content title="New Transaction" />
+      </Appbar.Header>
       <Text>Hello World!</Text>
     </Surface>
   );
