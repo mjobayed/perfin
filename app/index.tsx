bf3ede055f41426d10f856fda70a43f43be4591d
Md. Jobayed Islam <contact@mjobayed.com>
2026-07-01 13:26:54 +0600

    feat: fetch and show data from `AsyncStorage`
    Currently we display a very simple format of the data.
    Will add more robust display with cards and stuff.

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,38 @@
-import { useRouter } from "expo-router";
-import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
 import { AnimatedFAB, Appbar, Surface, Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { TxnData } from "@/types/types";
 
 export default function Index() {
   const insets = useSafeAreaInsets();
   const router = useRouter();
   const [isExtended, setIsExtended] = useState(true);
+  const [history, setHistory] = useState<TxnData[]>([]);
+
+  const loadHistory = async () => {
+    try {
+      const data = await AsyncStorage.getItem("transaction_history");
+      if (data) {
+        setHistory(JSON.parse(data));
+      }
+      console.log(history);
+    } catch (err) {
+      console.error("Failed to load transactions", err);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadHistory();
+    }, []),
+  );
+
+  const renderTxnItem = ({ item }: { item: TxnData }) => (
+    <Text>hello {item.txnId}</Text>
+  );
 
   return (
     <Surface style={[styles.rootSurface, { paddingBottom: insets.bottom }]}>
@@ -15,7 +40,11 @@ export default function Index() {
         <Appbar.Content title="Transactions" />
       </Appbar.Header>
 
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <FlatList
+        data={history}
+        keyExtractor={(item) => item.txnId.toString()}
+        renderItem={renderTxnItem}
+      />
       <AnimatedFAB
         icon={"plus"}
         label={"New Transaction"}
