45755cec82effad6735b22b9e14d71d67e2faa13
Md. Jobayed Islam <contact@mjobayed.com>
2026-07-12 19:59:54 +0600

    feat: wrap app with TxnProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
+import { TxnProvider } from "@/context/TxnContext";
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="new-transaction" options={{ headerShown: false }} />
-      </Stack>
+      <TxnProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="new-transaction"
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </TxnProvider>
     </PaperProvider>
   );
 }
