dcafe12100f99b20c083b1e5a83e8f14ac7773d1
Md. Jobayed Islam <contact@mjobayed.com>
2026-09-05 08:32:30 +0600

    fix: taps not being registered with keyboard open

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -324,6 +324,7 @@ export default function Index() {
         ]}
         ListEmptyComponent={renderEmptyState}
         onScroll={onListScroll}
+        keyboardShouldPersistTaps="handled"
       />
 
       <AnimatedFAB
